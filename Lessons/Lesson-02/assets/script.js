9e263732f173a494e47057ceb9bd3360f7864d69
mariannalonardi <mariannalonardi03@gmail.com>
2025-10-30 12:31:39 +0100

    half lesson

--- Lessons/Lesson-02/assets/script.js
+++ Lessons/Lesson-02/assets/script.js
@@ -2,3 +2,15 @@ const BUTTON = document.getElementById("button")
 
 const ELEMENT = document.querySelector(".myclass")
 const ELEMENTS = document.querySelectorAll(".myclass")
+
+// add a paragraph on button click
+BUTTON.addEventListener("click", () => {
+
+    let BLOCK = document.createElement("p")
+    BLOCK.textContent = "This is a new element!"
+    BLOCK.classList.add('test')
+
+    BLOCKS.appendChild(BLOCK) // append the element at the end
+    BLOCKS.prepend(BLOCK) // append the element at the beginning
+    
+})
